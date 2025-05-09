13f663ffa2582210345ad376e3a8d21c71ebe0eb
dongchan21 <dongchan5516@gmail.com>
2025-05-10 02:58:09 +0900

    지원

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import HomePage from "./HomePage";
 import AgePage from "./AgePage";
-import ResultPage from "./ResultPage";
 import FourBoxPage from "./FourBoxPage";
+import JiwonPage from "./JiwonPage";
 
 import "./App.css"
 import "./HomePage.css";
 import "./MainPage.css";
 import "./AgePage.css";
 import "./FourBoxPage.css";
+import "./JiwonPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<JiwonPage/>} />
+        {/* <Route path="/" element={<MainPage />} /> */}
         <Route path="/home" element={<HomePage />} />
         <Route path="/age" element={<AgePage />} />
         <Route path="/result" element={<FourBoxPage />} />
